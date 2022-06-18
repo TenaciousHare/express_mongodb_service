d171589f9ecb294fd6fb3bdfdd0ff5759105b375
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2022-06-18 12:17:05 +0200

    menu item

--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Serwis Express + MongoDB' });
+  res.render('news', { title: 'News' });
 });
 
 module.exports = router;
